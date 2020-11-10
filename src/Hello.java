c302e05386e2b296ac1ef2f599bd1663729a3388
Almira H <amiraa1225@gmail.com>
2020-11-10 14:51:37 -0500

    commiting again to see if its showing the files that i care about

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,5 @@
-package com.company;
-
-public class Main {
-
+public class Hello {
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Hello");
     }
 }
